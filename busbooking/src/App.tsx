d4a0e00b68df4aa8e7c3f02881e6df5ba3c7b767
taNMay9120 <tnmsinha65@gmail.com>
2025-05-26 22:07:29 +0530

    1st commit

---
+++ busbooking/src/App.tsx
@@ -0,0 +1,13 @@
+// import { MarqueeDemo } from "./components/demo";
+import { GlobeDemo } from "./components/globedemo";
+
+function App() {
+  return (
+    <>
+      <GlobeDemo />
+      {/* <MarqueeDemo /> */}
+    </>
+  );
+}
+
+export default App;
